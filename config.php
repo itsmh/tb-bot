f141717d7c73248c515db62e574cfc666af7e965
Masood Hamzehkhany <peace.mhy@gmail.com>
2017-11-22 16:39:55 +0330

    some changes

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+DEFINE ('DB_HOST' , getenv('DB_HOST') ?: 'localhost');
+DEFINE ('DB_USER' , getenv('DB_USER') ?: 'root');
+DEFINE ('DB_PASS' , getenv('DB_PASS') ?: '');
+DEFINE ('DB_NAME' , getenv('DB_NAME') ?: 'bot');
+
+DEFINE ('TELEGRAM_KEY' , getenv('TELEGRAM_KEY') ?: '152924976:AAE5uhhZvzlWzPuVPEyvWE0UYfbn6ylfXQs');
--- main.php
+++ main.php
@@ -1,36 +1,53 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/config.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Telegram\Bot\Api;
+use Medoo\Medoo;
 
-$telegram = new Api('152924976:AAE5uhhZvzlWzPuVPEyvWE0UYfbn6ylfXQs');
+$db = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => DB_NAME,
+    'server' => DB_HOST,
+    'username' => DB_USER,
+    'password' => DB_PASS
+        ]);
+$telegram = new Api(TELEGRAM_KEY);
 //var_dump($telegram->getMe());
 //var_dump( $telegram->getUpdates() );
 
-$keyboard = [
-    ['7', '8', '9'],
-    ['4', '5', '6'],
-    ['1', '2', '3'],
-         ['0']
-];
-
-$reply_markup = $telegram->replyKeyboardMarkup([
-	'keyboard' => $keyboard, 
-	'resize_keyboard' => true, 
-	'one_time_keyboard' => true
+$db->insert('chat_list', [
+    'user_id' => 2,
+    'chat_id' => 3,
+    'status'  => 0,
+    'create_time' => time()
 ]);
-
-$response = $telegram->sendMessage([
-	'chat_id' => '93442030', 
-	'text' => 'Hello World', 
-	'reply_markup' => $reply_markup
-]);
-
-$messageId = $response->getMessageId();
+echo $db->id();
+//
+//
+//$keyboard = [
+//        ['7', '8', '9'],
+//        ['4', '5', '6'],
+//        ['1', '2', '3'],
+//        ['0']
+//];
+//
+//$reply_markup = $telegram->replyKeyboardMarkup([
+//    'keyboard' => $keyboard,
+//    'resize_keyboard' => true,
+//    'one_time_keyboard' => true
+//        ]);
+//
+//$response = $telegram->sendMessage([
+//    'chat_id' => '93442030',
+//    'text' => 'Hello World',
+//    'reply_markup' => $reply_markup
+//        ]);
+//
+//$messageId = $response->getMessageId();
